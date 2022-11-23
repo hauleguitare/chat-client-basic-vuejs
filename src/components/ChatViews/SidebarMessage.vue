3165c5fc067b4221b87aa94221397f70841f4d3f
hauleguitare <alexander.dutyone@gmail.com>
2022-11-23 18:09:49 +0700

    add(): Create SidebarMessage component

---
+++ src/components/ChatViews/SidebarMessage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import SearchBar from "../SearchBar/SearchBar.vue";
+</script>
+
+<template>
+  <div class="sidebar-message-wrapper">
+    <SearchBar />
+  </div>
+</template>
+<style scoped lang="scss">
+.sidebar-message-wrapper {
+  @apply flex-grow-0 flex-shrink;
+  @apply bg-blue-400;
+  @apply px-2 py-2;
+}
+</style>
