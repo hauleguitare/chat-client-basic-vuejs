d9e3081448048b8ea991aa868d375957a27627f3
hauleguitare <alexander.dutyone@gmail.com>
2022-11-23 18:10:06 +0700

    add(): Create ChatView component

---
+++ src/views/ChatView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import SidebarMessage from "@/components/ChatViews/SidebarMessage.vue";
+import ChatViewWrapper from "@/components/ChatViews/ChatViewWrapper.vue";
+import ContentMessage from "@/components/ChatViews/ContentMessage.vue";
+</script>
+
+<template>
+  <ChatViewWrapper>
+    <SidebarMessage />
+    <ContentMessage />
+  </ChatViewWrapper>
+</template>
