f6ada2354ae8c319274730ceca9bd9b30d1acf46
hauleguitare <alexander.dutyone@gmail.com>
2022-11-23 18:08:40 +0700

    add(): Create ContentMessage component

---
+++ src/components/ChatViews/ContentMessage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="content-message-wrapper">Content message</div>
+</template>
+<style scoped lang="scss">
+.content-message-wrapper {
+  @apply flex-grow flex-shrink-0;
+  @apply bg-red-600;
+}
+</style>
