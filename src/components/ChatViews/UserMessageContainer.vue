abcc90f3e0e72e164a46fb656fee801779e8f3f8
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 16:29:28 +0700

    add(): Create UserMessage container component

---
+++ src/components/ChatViews/UserMessageContainer.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import Avatar from "../Account/Avatar.vue";
+import type { IUser } from "./SidebarMessage.vue";
+
+export interface IUserMessageProps extends IUser {}
+const props = defineProps<{ users: IUserMessageProps[] }>();
+</script>
+<template>
+  <div class="user-message-wrapper">
+    <div v-for="user of props.users" :key="user.username" class="user-message">
+      <Avatar :src="user.photoURL" size="46px" />
+      <div class="user-message--information">
+        <p class="user-message__username">{{ user.username }}</p>
+        <p class="user-message__message-review">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odio maiores itaque impedit
+          saepe aliquid ad accusantium quisquam accusamus ullam modi, magni obcaecati dolore
+          molestias vitae sed error eos. Voluptate...
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.user-message-wrapper {
+  @apply flex flex-col;
+  @apply mt-4;
+  @apply border-t-2;
+
+  .user-message {
+    @apply flex flex-row;
+    @apply items-center;
+    @apply py-2 px-2;
+
+    .user-message--information {
+      @apply flex-col flex-grow overflow-hidden;
+      @apply px-2;
+
+      .user-message__username {
+        @apply text-base;
+      }
+
+      .user-message__message-review {
+        @apply text-white/80 truncate;
+      }
+    }
+  }
+}
+</style>
