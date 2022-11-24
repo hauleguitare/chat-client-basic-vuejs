8f8543cad5ab73d2d48f71460eacb89734f23e6a
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 23:46:57 +0700

    add(): Configure color for dark theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: 'class',
   theme: {
     container: {
       center: true,
     },
-    extend: {},
+    extend: {
+      colors: {
+        "dark-theme-background": "#232424",
+        "dark-theme-surface": "#2A2B2A",
+        "dark-theme-on-surface": "#373737",
+        "dark-theme-primary": "#D5BD1E",
+        "dark-theme-secondary": "#3468B5",
+        "dark-theme-text-background": "#CCD0CD",
+        "dark-theme-text-surface": "#D7DBD8",
+      }
+    },
   },
   plugins: [],
 }
