a07bb29acf87fb9ff398e5ec0b2de000b562c611
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 16:28:04 +0700

    chore(): Change name AccountSidebarWrapper -> AccountSidebarContainer

---
+++ src/components/ChatViews/AccountSidebarContainer.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Avatar from "../Account/Avatar.vue";
+import AccountInformation from "../Account/AccountInformation.vue";
+</script>
+<template>
+  <div class="account-sidebar-wrapper">
+    <Avatar size="50px" />
+    <AccountInformation username="Welcome" numberPhone="XXX-XXXX-XXX" />
+  </div>
+</template>
+<style lang="scss" scoped>
+.account-sidebar-wrapper {
+  @apply flex flex-row;
+  @apply px-2 py-2;
+}
+</style>
