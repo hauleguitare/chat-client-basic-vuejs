d33d4d995ee217430dcfea2c14cac56dafb00192
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 15:30:02 +0700

    add(): Create account sidebar wrapper

---
+++ src/components/ChatViews/AccountSidebarWrapper.vue
@@ -0,0 +1,12 @@
+<script></script>
+<template>
+  <div class="account-sidebar-wrapper">
+    <slot />
+  </div>
+</template>
+<style>
+.account-sidebar-wrapper {
+  @apply flex flex-row;
+  @apply px-2 py-2;
+}
+</style>
