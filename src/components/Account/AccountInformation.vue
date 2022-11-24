28fa7213c0d52c826a7d3d3e6d35df796ba161ba
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 15:30:34 +0700

    add(): Create component for account information

---
+++ src/components/Account/AccountInformation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="account-information-box">
+    <p>{{ props.username }}</p>
+    <p>{{ props.numberPhone }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+export interface IAccountInformationProps {
+  username: string;
+  numberPhone: string;
+}
+const props = withDefaults(defineProps<IAccountInformationProps>(), {
+  username: "",
+  numberPhone: "XXX-XXXX-XXX",
+});
+</script>
+<style lang="scss" scoped>
+.account-information-box {
+  @apply text-base;
+  @apply font-mono;
+  @apply px-2 flex-grow;
+}
+</style>
