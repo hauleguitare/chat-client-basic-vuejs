5844663a1b7b17978e6d75757b3cc45be3069c03
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 15:30:51 +0700

    add(): create component for avatar

---
+++ src/components/Account/Avatar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AvatarGuest from "@/assets/avatar_guest.jpeg";
+export interface IAvatarProps {
+  size?: string | number;
+  src?: string;
+}
+const props = withDefaults(defineProps<IAvatarProps>(), { size: "32px", src: AvatarGuest });
+</script>
+
+<template>
+  <div class="box-avatar-wrapper" :style="{ width: props.size, height: props.size }">
+    <img :src="props.src" class="avatar-image" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.box-avatar-wrapper {
+  @apply overflow-hidden inline-block rounded-full;
+  .avatar-image {
+    @apply w-full h-full object-cover;
+  }
+}
+</style>
