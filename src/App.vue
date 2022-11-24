92aa12a7f42e8c944dab7b16b9c9364489142760
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 23:47:34 +0700

    refactor(): adding style for app component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
-<script setup lang="ts">
-import PageView from "./views/index.vue";
-import ChatView from "./views/ChatView.vue";
-</script>
-
 <template>
-  <div>
+  <div class="app-surface">
     <!-- <PageView /> -->
     <ChatView />
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.app-surface {
+  @apply dark:bg-dark-theme-background;
+}
+</style>
+
+<script setup lang="ts">
+import PageView from "./views/index.vue";
+import ChatView from "./views/ChatView.vue";
+</script>
