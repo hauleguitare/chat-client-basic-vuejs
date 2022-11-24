b519f2a44c3fd5cfedbe170c8247cb14d2c822d8
hauleguitare <alexander.dutyone@gmail.com>
2022-11-24 16:30:52 +0700

    refactor(): Adding UserMessageContainer, create array for test UI component

--- src/components/ChatViews/SidebarMessage.vue
+++ src/components/ChatViews/SidebarMessage.vue
@@ -1,32 +1,52 @@
 <script setup lang="ts">
-import Avatar from "../Account/Avatar.vue";
+export interface IUser {
+  photoURL: string;
+  username: string;
+}
+
 import SearchBar from "../SearchBar/SearchBar.vue";
-import AccountSideBarWrapper from "./AccountSidebarWrapper.vue";
-import AccountInformation from "../Account/AccountInformation.vue";
+import AccountSideBarContainer from "./AccountSidebarContainer.vue";
 import { useBreakPoints } from "@/utils/useBreakPoints";
+import UserMessageContainer from "./UserMessageContainer.vue";
 
 const [width, type, isMobile] = useBreakPoints();
+
+const users: IUser[] = [
+  {
+    photoURL:
+      "https://i.picsum.photos/id/68/200/200.jpg?hmac=CPg7ZGK1PBwt6DmjjPRApX_t-mOiYxt0pel50VH4Gwk",
+    username: "user1",
+  },
+  {
+    photoURL:
+      "https://i.picsum.photos/id/44/200/200.jpg?hmac=W5KcqhapHjBgEIHGQpQnX6o9jdOXQEVCKEdGIohjisY",
+    username: "user2",
+  },
+  {
+    photoURL:
+      "https://i.picsum.photos/id/852/200/200.jpg?hmac=4UHLpiS9j3YDnvq-w-MqnP5-ymiyvMs6BNV5ukoTRrI",
+    username: "user3",
+  },
+];
 </script>
 
 <template>
   <div class="sidebar-message-wrapper" :class="[width <= 1024 ? 'view-mobile' : 'view-desktop']">
-    <AccountSideBarWrapper>
-      <Avatar size="50px" />
-      <AccountInformation username="Welcome" numberPhone="XXX-XXXX-XXX" />
-    </AccountSideBarWrapper>
+    <AccountSideBarContainer />
     <SearchBar />
+    <UserMessageContainer :users="users" />
   </div>
 </template>
 
 <style scoped lang="scss">
 .sidebar-message-wrapper {
-  @apply flex-grow-0 flex-shrink transition-all ease-in duration-150;
+  @apply flex-grow-0 flex-shrink transition-all ease-in duration-150 max-w-[var(--widthSidebar)];
   @apply bg-blue-400;
   @apply px-2 py-2;
 }
 
 .view-mobile {
-  @apply invisible fixed inset-0 opacity-0 overflow-hidden -translate-x-full max-w-[var(--widthSidebar)];
+  @apply invisible fixed inset-0 opacity-0 overflow-hidden -translate-x-full;
 }
 
 .view-desktop {
