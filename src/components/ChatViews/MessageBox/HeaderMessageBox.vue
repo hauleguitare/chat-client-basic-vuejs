9305d267190b603b79f466bd90059d1c7d544a42
hauleguitare <alexander.dutyone@gmail.com>
2022-11-25 11:32:29 +0700

    chore(): Rename HeaderContent => HeaderMessageBox

--- src/components/ChatViews/MessageBox/HeaderMessageBox.vue
+++ src/components/ChatViews/MessageBox/HeaderMessageBox.vue
@@ -1,8 +1,10 @@
 <template><div class="header__content-message-container"></div></template>
-<style lang="scss">
+<style scoped lang="scss">
 .header__content-message-container {
   @apply w-full h-[var(--maxHeightHeaderChatView)];
-  @apply bg-dark-theme-surface;
+  @apply bg-dark-theme-on-surface;
+  @apply border-l-2 border-dark-theme-surface;
+  @apply flex-grow-0;
 }
 </style>
 <script setup lang="ts"></script>
