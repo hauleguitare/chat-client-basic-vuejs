964104ba7fb464c21ae3ac7d36d36e109bb9bfff
hauleguitare <alexander.dutyone@gmail.com>
2022-11-25 11:30:55 +0700

    chore(): Rename ContentMessage => ContentMessageBox

---
+++ src/components/ChatViews/MessageBox/ContentMessageBox.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="content__message-box-container">
+    <BubbleMessage />
+  </div>
+</template>
+<style lang="scss" scoped>
+.content__message-box-container {
+  @apply flex-grow;
+  @apply h-[calc(100vh_-_(var(--maxHeightHeaderChatView)_+_var(--heightMessagebar)))];
+}
+</style>
+<script setup lang="ts">
+import BubbleMessage from "@/components/BubbleMessage/BubbleMessage.vue";
+</script>
