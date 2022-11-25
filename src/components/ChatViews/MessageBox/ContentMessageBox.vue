ae77ec1f1e28e7124f779844eea13024de111868
hauleguitare <alexander.dutyone@gmail.com>
2022-11-25 12:36:31 +0700

    test(): Test UI for content message box

--- src/components/ChatViews/MessageBox/ContentMessageBox.vue
+++ src/components/ChatViews/MessageBox/ContentMessageBox.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="content__message-box-container">
-    <BubbleMessage />
+    <BubbleMessage msg="Hello" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage msg="5" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage msg="Hello" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage msg="Hello" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage msg="Hello" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage msg="Hello" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage msg="Hello" type="receive" />
+    <BubbleMessage msg="Hi" type="sent" />
+    <BubbleMessage msg="How are you" type="sent" />
   </div>
 </template>
 <style lang="scss" scoped>
 .content__message-box-container {
   @apply flex-grow;
-  @apply h-[calc(100vh_-_(var(--maxHeightHeaderChatView)_+_var(--heightMessagebar)))];
+  @apply max-h-[calc(100vh_-_(var(--maxHeightHeaderChatView)_+_var(--heightMessagebar)))];
+  @apply overflow-y-auto overscroll-y-contain overflow-x-hidden;
 }
 </style>
 <script setup lang="ts">
