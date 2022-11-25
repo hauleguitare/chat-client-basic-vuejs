d34931b6713f5083ca59237f68e947ca15e40e5f
hauleguitare <alexander.dutyone@gmail.com>
2022-11-25 12:58:37 +0700

    refactor(): adding style, component for Messagebar

--- src/components/ChatViews/MessageBox/MessageBar.vue
+++ src/components/ChatViews/MessageBox/MessageBar.vue
@@ -1,12 +1,43 @@
 <template>
-  <div class="message-bar">Message bar</div>
+  <div class="message-bar-wrapper">
+    <div class="message-bar--input-wrapper">
+      <input placeholder="Send message..." />
+    </div>
+    <button class="sender-icon--button">
+      <SenderIcon class="sender-icon" />
+    </button>
+  </div>
 </template>
 <style scoped lang="scss">
-.message-bar {
+.message-bar-wrapper {
   @apply flex-grow-0;
+  @apply flex flex-row;
   @apply w-full h-[var(--heightMessagebar)];
   @apply dark:bg-dark-theme-on-surface;
   @apply border-l-2 dark:border-dark-theme-surface;
+  .message-bar--input-wrapper {
+    @apply mx-4 my-2;
+    @apply bg-stone-600;
+    @apply overflow-hidden rounded-full;
+    @apply flex-grow;
+    input {
+      @apply bg-transparent outline-none;
+      @apply w-full h-full;
+      @apply py-2 px-4;
+      @apply text-sm;
+    }
+  }
+}
+.sender-icon--button {
+  @apply px-1 py-1;
+}
+.sender-icon {
+  @apply fill-dark-theme-primary;
+  @apply stroke-dark-theme-background stroke-1;
+  @apply w-8 h-8;
+  @apply inline-block;
 }
 </style>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SenderIcon from "@/components/icons/SenderIcon.vue";
+</script>
