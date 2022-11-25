38bc0071eede5c71ab222da63535eca3a0c39193
hauleguitare <alexander.dutyone@gmail.com>
2022-11-25 11:34:57 +0700

    add(): Create MessageBar component

---
+++ src/components/ChatViews/MessageBox/MessageBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="message-bar">Message bar</div>
+</template>
+<style scoped lang="scss">
+.message-bar {
+  @apply flex-grow-0;
+  @apply w-full h-[var(--heightMessagebar)];
+  @apply dark:bg-dark-theme-on-surface;
+  @apply border-l-2 dark:border-dark-theme-surface;
+}
+</style>
+<script setup lang="ts"></script>
