65e610e3d42fcc385bf734b06ce577e7c7ab6798
hauleguitare <alexander.dutyone@gmail.com>
2022-11-26 11:21:41 +0700

    refactor(): Adding TransitionGroup for listening event virtual DOM

--- src/components/ChatViews/MessageBox/ContentMessageBox.vue
+++ src/components/ChatViews/MessageBox/ContentMessageBox.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="content__message-box-wrapper">
     <BubbleMessage msg="Hello" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
-    <BubbleMessage msg="5" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
-    <BubbleMessage msg="Hello" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
-    <BubbleMessage msg="Hello" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
-    <BubbleMessage msg="Hello" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
-    <BubbleMessage msg="Hello" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
-    <BubbleMessage msg="Hello" type="receive" />
-    <BubbleMessage msg="Hi" type="sent" />
-    <BubbleMessage msg="How are you" type="sent" />
+    <BubbleMessage
+      msg="Welcome to Bundles Chat, how are you today?, Do you wanna chat with me?"
+      type="receive"
+    />
+    <TransitionGroup name="list">
+      <BubbleMessage
+        v-for="(message, index) of dataMessage"
+        :key="index"
+        :msg="message"
+        type="sent"
+      />
+    </TransitionGroup>
   </div>
 </template>
 <style lang="scss" scoped>
@@ -29,7 +21,22 @@
   @apply max-h-[calc(100vh_-_(var(--maxHeightHeaderChatView)_+_var(--heightMessagebar)))];
   @apply overflow-y-auto overscroll-y-contain overflow-x-hidden;
 }
+
+.list-enter-active,
+.list-leave-active {
+  @apply transition duration-500;
+}
+.list-enter-from,
+.list-leave-to {
+  @apply translate-y-[30%] opacity-0;
+}
 </style>
 <script setup lang="ts">
 import BubbleMessage from "@/components/BubbleMessage/BubbleMessage.vue";
+
+export interface IContentMessageBox {
+  dataMessage: Array<string>;
+}
+
+const { dataMessage } = defineProps<IContentMessageBox>();
 </script>
